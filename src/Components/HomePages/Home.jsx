07fa2c6988eed0458638156caac147034fb70c39
rudraksh-sharma-1 <rudraksh2233rr@gmail.com>
2024-07-26 21:09:20 +0530

    Update Home component, NavBar, and styles
    - Modified src/Components/HomePages/Home.jsx to add new features and improvements
    - Updated src/Components/NavBar.jsx for better navigation functionality
    - Improved src/Components/Style.css for underline animation on nav links
    
    This update enhances the user experience and fixes various UI issues.

--- src/Components/HomePages/Home.jsx
+++ src/Components/HomePages/Home.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import Slider from './Slider';
-import Inventory from './Inventory';
-import OurStory from './OurStory';
-import Contactus from './ContactUs';
-import HomeImage from '../Images/BackgroundImage.avif'
-import './HomeStyle.css';
+import React from "react";
+import Slider from "./Slider";
+import Inventory from "./Inventory";
+import OurStory from "./OurStory";
+import Contactus from "./ContactUs";
+import "./HomeStyle.css";
 
 function Home() {
   return (
     <div>
-      <img className='Home-Page-Image' src={HomeImage} alt="Home Candle Image" />
-      <Slider/>
-      <Inventory/>
-      <OurStory/>
-      <Contactus/>
+      <Slider />
+      <Inventory />
+      <OurStory />
+      <Contactus />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,31 +1,55 @@
-import React from 'react'
-import './Style.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Style.css";
+import { Link } from "react-router-dom";
 function NavBar() {
   return (
-    <nav className='Nav-Main-Container' data-aos="fade-down">
-      
+    <nav className="Nav-Main-Container" data-aos="fade-down">
       {/* Nav-Bar-Logo */}
-      <h1 className='Nav-Bar-Logo'><Link to='/'>Logo</Link></h1>
+      <h1 className="Nav-Bar-Logo">
+        <Link to="/">Logo</Link>
+      </h1>
 
       {/* Nav-Bar-List */}
-      <div className='Nav-List'>
+      <div className="Nav-List">
         <ul>
-          <li><Link className='Nav-List-Link'>Candles</Link></li>
-          <li><Link className='Nav-List-Link'>Gifts</Link></li>
-          <li><Link className='Nav-List-Link'>Our Story</Link></li>
-          <li><Link className='Nav-List-Link'>Contact Us</Link></li>
+          <li>
+            <Link className="Nav-List-Link">Candles</Link>
+          </li>
+          <li>
+            <Link className="Nav-List-Link">Gifts</Link>
+          </li>
+          <li>
+            <Link className="Nav-List-Link">Our Story</Link>
+          </li>
+          <li>
+            <Link className="Nav-List-Link">Contact Us</Link>
+          </li>
         </ul>
       </div>
 
       {/* Nav-Bar-Icons */}
-      <div className='Nav-Icons'>
-      <Link><i class="bi bi-person" style={{fontSize: 27, color:'lightsteelblue', paddingRight: 8}}></i></Link>
-      <Link><i class="bi bi-search" style={{fontSize: 24, color:'lightsteelblue', paddingRight: 8}}></i></Link>
-      <Link><i class="bi bi-cart2" style={{fontSize: 28, color: 'lightsteelblue', paddingRight: 8}}></i></Link>
+      <div className="Nav-Icons">
+        <Link>
+          <i
+            class="bi bi-person"
+            style={{ fontSize: 30, color: "lightsteelblue", paddingRight: 8 }}
+          ></i>
+        </Link>
+        <Link>
+          <i
+            class="bi bi-search"
+            style={{ fontSize: 24, color: "lightsteelblue", paddingRight: 8 }}
+          ></i>
+        </Link>
+        <Link>
+          <i
+            class="bi bi-cart2"
+            style={{ fontSize: 28, color: "lightsteelblue", paddingRight: 8 }}
+          ></i>
+        </Link>
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
