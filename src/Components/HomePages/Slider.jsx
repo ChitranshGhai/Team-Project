8b2f44f3733e3b2da52fd0dd3a3f243c6e396d27
rudraksh-sharma-1 <rudraksh2233rr@gmail.com>
2024-07-26 22:39:59 +0530

    Update HomePage components and images
    - Modify HomeStyle.css to adjust styles for the HomePage.
    - Update Slider.jsx to enhance functionality and performance.
    - Remove BackgroundImage.avif and background.png due to redundancy.
    - Add new slide images: Slide1.webp, Slide2.jpg, and Slide3.webp for the slider.
    - Update Style.css to reflect changes in the overall design and layout.

--- src/Components/HomePages/Slider.jsx
+++ src/Components/HomePages/Slider.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Slide1 from "../Images/Slide1.webp";
+import Slide2 from "../Images/Slide2.jpg";
+import Slide3 from "../Images/Slide3.webp";
 
 function Slider() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <div>
-      <h1>This is Slider Section</h1>
-    </div>
-  )
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className="Slide-Image" src={Slide1} alt="First Slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="Slide-Image" src={Slide2} alt="Second Slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="Slide-Image" src={Slide3} alt="Third Slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default Slider
+export default Slider;
