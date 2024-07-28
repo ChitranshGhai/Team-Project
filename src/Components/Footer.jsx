5feec03c7a6dda0dc7509b99cfa0df1b839d8c5d
ChitranshGhai <chitranshghai@gmail.com>
2024-07-28 21:44:13 +0530

    Updated Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,22 +10,21 @@ function Footer() {
       <footer className='Footer'>
       <div class="footer-section">
           <h3>Contact</h3>
-          <p>T: +91 9871580964</p>
+          <p>P: +91 9871580964</p>
           <p>IG: <a href="https://www.instagram.com/sirimiricandles/" target="_blank">@sirimiricandles</a></p>
           <p><a href="">customerservice@sirimiri.in</a></p>
       </div>
       <div class="footer-section">
           <h3>Services</h3>
-          <p><a href="#">Exclusive offers</a></p>
-          <p><a href="#">Corporate sales</a></p>
-          <p><a href="#">Store location</a></p>
+          <p><a href="#">Exclusive Offers</a></p>
+          <p><a href="#">Corporate Sales</a></p>
           <p><a href="#">Gifts</a></p>
       </div>
       <div class="footer-section">
           <h3>Orders</h3>
-          <p><a href="#">My account</a></p>
-          <p><a href="#">Delivery information</a></p>
-          <p><a href="#">Track my order</a></p>
+          <p><a href="#">My Orders</a></p>
+          <p><a href="#">Policies</a></p>
+          <p><a href="#">FAQ's</a></p>
           <p><a href="#">Help</a></p>
       </div>
       <div class="footer-section newsletter-section">
