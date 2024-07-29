bb4b618996ec17c00d9b2961c1c057b70cdb94bc
ChitranshGhai <chitranshghai@gmail.com>
2024-07-29 09:33:41 +0530

    Update App component to use Router and add ScrollToTop functionality
    - Modified App.js to use BrowserRouter as Router for route management.
    - Added new ScrollToTop component to ensure the page scrolls to the top on route change.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Home from './Components/HomePages/Home';
 import OurStory from './Components/OurStoryPage/ourStory';
 import Footer from './Components/Footer';
+import ScrollToTop from './Components/ScrollToTop';
+
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/OurStory" element={<OurStory/>}></Route>
-        <Route></Route>
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <Router>
+      <NavBar />
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/OurStory" element={<OurStory />} />
+        </Routes>
+      </ScrollToTop>
+      <Footer />
+    </Router>
     </>
   );
 }
  
-export default App;
+export default App;   
