586bfe8d17706a9b28854266ca2ae743dcbc57ff
Vaibhavi-Sharma <vaibhavi1764@gmail.com>
2024-07-30 12:55:20 +0530

    shop page updated

--- src/Components/CollectionPages/ShopPage.jsx
+++ src/Components/CollectionPages/ShopPage.jsx
@@ -4,31 +4,31 @@ import './shop_page.css'
 import candle1 from '../Images/gift2.jpg'
 import candle2 from '../Images/gift3.jpg'
 export default function ShopPage() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
   return (
-    <>
-      <div className='container'>
+    <div id='whole'>
+      <div id='container'>
         <div className="content-wrap">
           <div className="side-area">
-            <img src={candle} alt="" className='first' />
-            <img src={candle} alt="" className='first' />
-            <img src={candle} alt="" className='first' />
+            <img src={candle} alt="" className='firstimg' />
+            <img src={candle} alt="" className='firstimg' />
+            <img src={candle} alt="" className='firstimg' />
           </div>
           <div className="main-content">
             <img src={candle} alt="" className='main' />
             <div className="content">
               <h1 className='product-name'>CANDLE NAME</h1>
               <p className="product-price">Rs. 2000</p>
-              <div className="review">
+              {/* <div className="review">
                 <span className='stars'>★★★★★</span>
                 <span className="text">(1 review review)</span>
-              </div>
+              </div> */}
               <p className="product-desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab velit delectus itaque sequi veritatis necessitatibus dicta vitae quod, vel accusamus voluptatem nesciunt fugit! Dolor iure, odio corporis itaque natus inventore eos! Quasi eveniet numquam facilis, tempore, aperiam omnis qui veniam, illo voluptatibus obcaecati quod provident nostrum. Voluptate, molestiae corrupti.
               </p>
               <div className="counter">
                 <button onClick={() => { setCount(count - 1) }}>-</button>
-                <h3>{count}</h3>
+                <h3 className='height3'>{count}</h3>
                 <button onClick={() => { setCount(count + 1) }}>+</button>
               </div>
               <div className="button-group">
@@ -43,7 +43,7 @@ export default function ShopPage() {
           <h4>Additional Information</h4>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum quae rerum velit, atque id blanditiis et cum iure? Doloremque cumque earum suscipit natus iusto similique dolorem consectetur magnam blanditiis totam?</p>
         </div>
-        <h3>Related Product</h3>
+        <h3 className='height'>Related Product</h3>
         <div className="related-prod">
         <div className='image-wrapper col-3'>
             <div className="image-inner-wrapper">
@@ -80,6 +80,6 @@ export default function ShopPage() {
           </div>
         </div>
 
-    </>
+    </div>
   )
 }
