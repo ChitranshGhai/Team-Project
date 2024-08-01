3088596edf1c1897e7c155e501532e7a5f913f9f
ChitranshGhai <chitranshghai@gmail.com>
2024-08-01 20:02:59 +0530

    Merge branch 'main' of https://github.com/rudraksh-sharma-1/Team-Project

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Style.css";
 import { Link } from "react-router-dom";
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav
-      className="Nav-Main-Container container-fluid" /* data-aos="fade-down" */
-    >
+    <nav className={`Nav-Main-Container ${menuOpen ? 'active' : ''}`}>
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
       {/* Nav-Bar-Logo */}
       <h2 className="Nav-Bar-Logo">
         <Link to="/">Logo</Link>
@@ -15,47 +25,38 @@ function NavBar() {
       <div className="Nav-List">
         <ul>
           <li>
-            <Link className="Nav-List-Link">Candles</Link>
+            <Link className="Nav-List-Link" to="/Candles">Candles</Link>
           </li>
           <li>
-            <Link className="Nav-List-Link">Gifts</Link>
+            <Link className="Nav-List-Link" to="/Accessories">Bulk Order Enquiry</Link>
           </li>
           <li>
-            <Link className="Nav-List-Link">Accessories </Link>
+            <Link className="Nav-List-Link" to="/Collections">Collections</Link>
           </li>
           <li>
-            <Link to="/Collections" className="Nav-List-Link">
-              Collections
-            </Link>
+            <Link className="Nav-List-Link" to="/OurStory">Our Story</Link>
           </li>
           <li>
-            <Link to="/OurStory" className="Nav-List-Link">
-              Our Story
-            </Link>
-          </li>
-          <li>
-            <Link to="/ContactUs" className="Nav-List-Link">
-              Contact Us
-            </Link>
+            <Link className="Nav-List-Link" to="/ContactUs">Contact Us</Link>
           </li>
         </ul>
       </div>
 
       {/* Nav-Bar-Icons */}
       <div className="Nav-Icons">
-        <Link to="/Login">
+        <Link to="/profile">
           <i
             className="bi bi-person"
             style={{ fontSize: 32, color: "lightsteelblue", paddingRight: 8 }}
           ></i>
         </Link>
-        <Link>
+        <Link to="/search">
           <i
             className="bi bi-search"
             style={{ fontSize: 24, color: "lightsteelblue", paddingRight: 8 }}
           ></i>
         </Link>
-        <Link>
+        <Link to="/cart">
           <i
             className="bi bi-cart2"
             style={{ fontSize: 28, color: "lightsteelblue", paddingRight: 8 }}
