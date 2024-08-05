9d5278cb95fc09afa39decd835087a61592bc457
ChitranshGhai <chitranshghai@gmail.com>
2024-08-05 13:23:18 +0530

    -Fixed HomePage doing out of page

--- Client/src/Components/HomePages/OurStory.jsx
+++ Client/src/Components/HomePages/OurStory.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 function OurStory() {
    return (
     <div className="container-fluid Our-Story-main-Container d-flex align-item-center justify-content-around " /* border border-2 border-danger */>
-        <p id="OurStory-Para" data-aos="fade-right">
+        <p id="OurStory-Para" data-aos="fade-up" data-aos-delay="1000" data-aos-duration="1000">
           <h2>Our Story</h2>
           Sirimiri Candles, founded by Geetansha Ghai, is a vibrant startup dedicated to creating natural scented candles. Inspired by a passion for aromatherapy and a desire to promote wellness. Geetansha embarked on this entrepreneurial journey to craft candles that are not only aromatic but also beneficial for mental and physical health...
           <br />
           <br />
           <Link to="/OurStory" id="OurStoryBtnLink"><button id="OurStory-Button">Read Our Story</button></Link> 
         </p>
-        <img className="CardBoxShadow" id="Ourstory-Img" src={OurStorypic} alt="candle picture" data-aos="fade-left" />
+        <img className="CardBoxShadow" id="Ourstory-Img" src={OurStorypic} alt="candle picture" data-aos="fade-up" data-aos-delay="1000" data-aos-duration="1000" />
       </div>
   );
 }
