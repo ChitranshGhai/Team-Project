861b4acac154992e4ae6a47868242e13533f7c4c
Arjunfellasleep <panda8376967799@gmail.com>
2024-08-15 21:18:54 +0530

    feat: Update Slider component and add new images
    - Modified Client/src/Components/HomePages/Slider.jsx for improved functionality
    - Added new image files: Collectionpage.png, FLASHSALE.png, and Welcomepage.png

--- Client/src/Components/HomePages/Slider.jsx
+++ Client/src/Components/HomePages/Slider.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import Slide1 from "../Images/Slide1.webp";
+import Collectionpage from "../Images/Collectionpage.png"
+import Welcomepage from "../Images/Welcomepage.png"
+import FLASHSALE from "../Images/FLASHSALE.png"
 
 function Slider() {
   const [index, setIndex] = useState(0);
@@ -16,21 +18,21 @@ function Slider() {
       /* className="border border-2 border-danger mt-3" */
     >
       <Carousel.Item>
-        <img className="Slide-Image" src={Slide1} alt="First Slide" />
+        <img className="Slide-Image" src={Welcomepage} alt="First Slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="Slide-Image" src={Slide1} alt="Second Slide" />
+        <img className="Slide-Image" src={FLASHSALE} alt="Second Slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="Slide-Image" src={Slide1} alt="Third Slide" />
+        <img className="Slide-Image" src={Collectionpage} alt="Third Slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
